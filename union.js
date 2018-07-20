635e45b5185598ba1df1bf87d6522c5e0594b0bb
Devoxin <lukel9914@gmail.com>
2018-07-20 23:49:56 +0100

    Duh, if we have new messages then it won't scroll because it's not at the bottom

--- union.js
+++ union.js
@@ -274,9 +274,9 @@ function snedMeHarder (event) {
 
 function scrollToBottom() {
   const container = document.getElementById('message-container');
-  const isAtBottom = container.scrollTop === (container.scrollHeight - container.offsetHeight);
+  const shouldScroll = container.scrollTop > (container.scrollHeight - container.offsetHeight) - 100;
 
-  if (isAtBottom) {
+  if (shouldScroll) {
     container.scrollTop = container.scrollHeight;
   }
 }
