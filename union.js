7e63421b2d4f634573279c0a9cef86b3008abbab
Devoxin <lukel9914@gmail.com>
2018-07-28 16:00:56 +0100

    Cleaning up stuffs

--- union.js
+++ union.js
@@ -65,22 +65,18 @@ async function signup () {
 
 function connect () {
   document.getElementById('login').style.display = 'none';
-  ws = new WebSocket('wss://union.serux.pro:2096');
-  ws.onopen = authenticateClient; // Stupid JS Websocket doesn't support headers REEEEEEEEE
-  ws.onclose = handleWSClose;
-  ws.onmessage = handleWSMessage;
-}
 
-function authenticateClient () {
-  if (_auth !== null) {
-    currentUser = atob(_auth).split(':')[0];
-  } else {
-    const [username, password] = ['username', 'password'].map(id => document.getElementById(id).value);
+  const [username, password] = _auth !== null
+    ? atob(_auth).split(':')
+    : ['username', 'password'].map(id => document.getElementById(id).value);
+
+  if (_auth === null) {
     _auth = btoa(`${username}:${password}`);
-    currentUser = username;
   }
 
-  ws.send(`Basic ${_auth}`);
+  ws = new WebSocket(`wss://${username}:${password}@union.serux.pro:2096`);
+  ws.onclose = handleWSClose;
+  ws.onmessage = handleWSMessage;
 }
 
 function handleWSClose (close) {
@@ -200,6 +196,7 @@ function handleWSMessage (message) {
 
     if (j.op === INBOUND_OPCODES.Hello) { // hello
       localStorage.setItem('token', _auth);
+      currentUser = atob(_auth).split(':')[0];
 
       if ('Notification' in window && Notification.permission === 'default') {
         Notification.requestPermission();
@@ -296,7 +293,6 @@ function handleWSMessage (message) {
         }
       }
     }
-
 
   } catch(e) {
     console.log(e);
